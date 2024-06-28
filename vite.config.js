6bf7499df2f292805d5cd80d40126837c3d87d45
Mateusz Muślewski <mateusz.mus@proton.me>
2024-06-28 13:04:24 +0000

    Pending changes exported from your codespace

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/polmed-router/',
   plugins: [react()],
   test: {
     globals: true,
