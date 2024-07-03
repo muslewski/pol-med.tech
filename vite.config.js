ac191db2ad95b37505e13b8e1574e7118ae1242e
Mateusz Muślewski <mateusz.mus@proton.me>
2024-07-03 12:06:04 +0000

    repair links

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/polmed-tech.com/",
+  base: "/pol-med.tech/",
   plugins: [react()],
   test: {
     globals: true,
