48c9cefd918430449bfb098625775614cf4c3e49
KentoDecem <10kento10@gmail.com>
2024-08-14 18:19:50 +0200

    Updates

--- assets/Copyright-492a196f.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ccc1ad89.js";function i(){return r.jsx("div",{children:"Copyright"})}export{i as default};
---
+++ assets/Copyright-a8f039fb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-984c726f.js";function i(){return r.jsx("div",{children:"Copyright"})}export{i as default};
