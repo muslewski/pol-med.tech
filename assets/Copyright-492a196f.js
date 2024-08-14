655539ccf1f75d5f856cb626de7124a8fadd79a4
KentoDecem <10kento10@gmail.com>
2024-08-14 18:12:12 +0200

    Updates

---
+++ assets/Copyright-492a196f.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ccc1ad89.js";function i(){return r.jsx("div",{children:"Copyright"})}export{i as default};
--- assets/PrivacyPolicy-3915f876.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-e97e0926.js";function o(){return i.jsx("div",{children:"PrivacyPolicy"})}export{o as default};
