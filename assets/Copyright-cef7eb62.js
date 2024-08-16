22f91934a9f991939c9975f13b5fafc37585638d
KentoDecem <10kento10@gmail.com>
2024-08-16 23:03:29 +0200

    Updates

--- assets/Copyright-cef7eb62.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-092b03fd.js";function i(){return r.jsx("div",{children:"Copyright"})}export{i as default};
--- assets/PrivacyPolicy-a3ff22b0.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-092b03fd.js";function o(){return i.jsx("div",{children:"PrivacyPolicy"})}export{o as default};
