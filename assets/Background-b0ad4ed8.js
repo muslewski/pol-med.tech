775df57be78f34218872135f50d4423e62f6957c
KentoDecem <10kento10@gmail.com>
2024-09-21 17:57:20 +0200

    Updates

--- assets/Background-b0ad4ed8.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-f49ad048.js";function a({image:n,bgColor:e=" bg-secondary-dark",alt:o="Background Image"}){return r.jsx("img",{className:"background pointer-events-none select-none",src:n,alt:o})}export{a as B};
